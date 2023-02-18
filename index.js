b1ac18ab6fc3eae0333ac740aafdffa22e72a26e
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-02-18 12:21:48 -0500

    Subiendo proyecto al repositorio

---
+++ index.js
@@ -0,0 +1,28 @@
+const cheerio = require('cheerio');
+const request = require('request-promise');
+const fs = require('fs-extra');
+
+const writeStream = fs.createWriteStream('quotes.csv')
+
+async function init(){
+    
+    const $ = await request({
+        uri: 'http://quotes.toscrape.com/',
+        transform: body => cheerio.load(body)
+    });
+
+    writeStream.write('Quote|Autor|Tags\n')
+
+    $('.quote').each((i, el) => {
+        const text = $(el).find('span.text').text().replace(/(^\“|\”$)/g,"");
+        const author = $(el).find('span small.author').text();
+        const tags = [];
+        $(el).find('.tags a.tag').each((i, el) => tags.push($(el).text()))
+        /* console.log(text, author, tags) */
+        /* console.log(text, author, tags.join(',')) */
+        writeStream.write(`${text}|${author}|${tags}\n`)
+    })
+    
+}
+
+init();
