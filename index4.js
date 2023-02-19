1ac61bda53333dd940c2888cf9aecf263d1d1b24
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-02-19 14:49:19 -0500

    Extraccion 4 y 5 Ok

--- index4.js
+++ index4.js
@@ -1,13 +1,12 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
-
 const fs = require('fs-extra');
 
 const writeStream = fs.createWriteStream('ebooks.csv')
 
-
 async function init() {
     const $ = await request({
+         // URL de la página a scrappear
         uri: 'https://www.buscalibre.pe/',
         transform: body => cheerio.load(body)
     });
@@ -15,12 +14,12 @@ async function init() {
     writeStream.write('Libro|Autores|Ventas|Precio\n')
 
     $('.box-producto').each((i, el) => {
-        const title = $(el).find('h3.nombre').text();
-        const autor = $(el).find('.metas').text();
-        const autores = autor.replace(/(\r\n|\n|\r)/g,'');
+        const title = $(el).find('h3.nombre').text() || 'No hay Titulo';
+        const autor = $(el).find('.metas').text() || 'No hay Autor';
+        const autores = autor.trim();
         /* const rating = $(el).find('span.small starts').text(); */
         const ventas =  $(el).find('span.font-size-small').text() || '0';
-        const price = $(el).find('p.precio-ahora').text();
+        const price = $(el).find('p.precio-ahora').text() || 'S/ 0.00';
         console.log(`${title}|${autores}|${ventas}|${price}\n`);
         writeStream.write(`${title}|${autores}|${ventas}|${price}\n`);
     })
---
+++ index5.js
@@ -0,0 +1,28 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+const fs = require('fs-extra');
+
+const writeStream = fs.createWriteStream('crisol.csv')
+
+async function init() {
+    const $ = await request({
+        // URL de la página a scrappear
+        uri: 'https://www.crisol.com.pe/los-mas-vendidos',
+        transform: body => cheerio.load(body)
+    });
+
+    writeStream.write('Imagen|Producto|Autor|Precio\n')
+
+    $('.product-item-info').each((i, el) => {
+        const img = $(el).find('img.product-image-photo ').attr('src') || 'No hay imagen';
+        const producto = $(el).find('div.product strong a').text().trim() || 'Sin nombre';
+        const autor = $(el).find('div.author').text().trim() || 'Sin Autor';
+        const precio = $(el).find('span.price').text().trim() || 'Sin Precio';
+        console.log(`${img}|${producto}|${autor}|${precio}\n`)
+        writeStream.write(`${img}|${producto}|${autor}|${precio}\n`);
+    })
+
+}
+
+init();
+
